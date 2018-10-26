04e8b435091b7b506c85984a5054b57869b81abf
Zhou <18555037835@163.com>
2018-10-26 16:49:39 +0700

    考研论坛

---
+++ 考研论坛/js/header-bbs.js
@@ -0,0 +1,9 @@
+document.writeln("<div class=\"kyLogo\"><a href=\"index.html\">唐院考研网<i><\/i><\/a><\/div>");
+document.writeln("<ul class=\"hdkyList\">");
+document.writeln("<li><a href=\"index.html\" title=\"考研网\" target=\"_self\">首页<\/a><\/li>");
+
+document.writeln("<li class=\"kyGap\"><\/li>");
+document.writeln("<li class=\"kyCur\"><a href=\"index.html\" title=\"考研论坛\" target=\"_self\">论坛<\/a><\/li>");
+document.writeln("<li class=\"kyGap\"><\/li>");
+document.writeln("<li><a href=\"index.html\" title=\"考研资料\" target=\"_self\">资料<\/a><\/li>");
+document.writeln("<\/ul>");
